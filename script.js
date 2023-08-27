afe30647b984d1a733f6804d18c2d956ad6a229e
Alejandra Camacho <alejandra.e.bernabel@gmail.com>
2023-08-27 15:55:09 -0400

    Finished connecting weather API. Responses still need to be formatted.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var fetchButton = document.getElementById("fetchButton");
 
-
 function getLatLong (cityEntered) {
     let latLongUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityEntered + "&limit=1&appid=ca007cce6aac096fab36f61f7e4396a9";
 
@@ -19,7 +18,13 @@ function getLatLong (cityEntered) {
 
 function getWeather(lat, lon) {
     //open weather needs lat, lon, and API key.
-    let requestURL = "api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon=" + lon + "&appid=ca007cce6aac096fab36f61f7e4396a9";
+
+    console.log ("longitude is " + lon);
+    console.log ("latitude is " + lat);
+
+    // let requestURL = "api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon=" + lon + "&appid=ca007cce6aac096fab36f61f7e4396a9";
+    let requestURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=ca007cce6aac096fab36f61f7e4396a9";
+    console.log(requestURL);
 
     fetch(requestURL)
     .then (function (response){
@@ -27,14 +32,34 @@ function getWeather(lat, lon) {
     })
     
     .then (function (data){
-        console.log ("longitude is " + data[0].lon);
-        console.log ("latitude is " + data[0].lat);
+        console.log(data);
+    })
 
+    .catch (error => {
+        console.log ("There was an error and location weather information was not retrieved.");
     });
 }
 
+fetchButton.addEventListener("click", function () {
+    let location = document.getElementById("cityWeather");
+    console.log(location.value);
+    getLatLong(location.value);
+});
 
+// function exampleWeatherSearch () {
+//     // let requestURL = "api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=ca007cce6aac096fab36f61f7e4396a9";
+//     // console.log(requestURL);
 
+//     fetch("http://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=ca007cce6aac096fab36f61f7e4396a9")
+//     .then (function (response){
+//         return response.json();
+//     })
+    
+//     .then (function (data){
+//         console.log(data);
+//     })
 
-
-fetchButton.addEventListener("click", getWeather);
+//     .catch (error => {
+//         console.log ("There was an error and location weather information was not retrieved.");
+//     });
+// }
