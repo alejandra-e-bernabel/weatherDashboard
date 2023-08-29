4239a2f2b69d12e3079ece70793935e9093b50c0
Alejandra Camacho <alejandra.e.bernabel@gmail.com>
2023-08-29 13:42:36 -0400

    Finished forecast cards.

--- script.js
+++ script.js
@@ -82,7 +82,75 @@ function printDayData (data, i) {
     let dateYear = date.substring(0,4);
     let dateMonth = date.substring(6,7);
 
-    // changes date to spelled out month
+    //changes month from number to spelled out month
+    dateMonth = getMonth(dateMonth);
+
+    let dateDay = date.substring(8,10);
+
+    // console.log ("Icon: " + data.list[i].weather[i].icon);
+
+    cityInfo.innerHTML = "<div class=\"cityHeader\" id=cityHeader></div>";
+
+    const img = document.createElement("img");
+    img.src = "https://openweathermap.org/img/wn/" + data.list[i].weather[i].icon + ".png";
+    document.getElementById("cityHeader").append(img);
+
+    // document.createElement("h1");
+    document.getElementById("cityHeader").innerHTML += "<h1>" + data.city.name + "</h1>";
+
+    cityInfo.innerHTML +="<h4>" + dateMonth + " " + dateDay + ", " + dateYear + "</h4>";
+    cityInfo.innerHTML += "<p>Current temperature: " + data.list[i].main.temp + "\u00b0 F</p>";
+    cityInfo.innerHTML += "<p>Feels like: " + data.list[i].main.feels_like + "\u00b0 F</p>";
+    cityInfo.innerHTML += "<p>Humidity: " + data.list[i].main.humidity + "%</p>";
+    cityInfo.innerHTML += "<p>Today's Low: " + data.list[i].main.temp_min + "\u00b0 F</p>";
+    cityInfo.innerHTML += "<p>Today's High: " + data.list[i].main.temp_max + "\u00b0 F</p>";
+    cityInfo.innerHTML += "<p>Wind Speed: " + data.list[i].wind.speed + "mph</p>";
+
+}
+
+function printForecastCards (data) {
+    //will grab only data indices for future days at 12:00pm.
+    let forecastDays = [5, 13, 21, 29, 37];
+
+    for (i=0 ; i<forecastDays.length; i++) {
+        const forecastCard = document.createElement("div");
+
+        let date= data.list[forecastDays[i]].dt_txt;
+        let dateYear = date.substring(0,4);
+        let dateMonth = date.substring(6,7);
+
+        //changes month from number to spelled out month
+        dateMonth = getMonth(dateMonth);
+
+        let dateDay = date.substring(8,10);
+
+        const forecastCardHeader = document.createElement("div");
+        forecastCardHeader.classList.add("forecastCardHeader");
+        forecastCardHeader.id = "forecastCardHeader";
+        forecastCard.append(forecastCardHeader);
+
+        let forecastImg = document.createElement("img");
+        forecastImg.src = "https://openweathermap.org/img/wn/" + data.list[forecastDays[i]].weather[0].icon + ".png";
+
+        //adds image and date to forecastCardHeader
+        forecastCardHeader.append(forecastImg);
+        forecastCardHeader.innerHTML += "<h5>" + dateMonth + " " + dateDay + ", " + dateYear + "</h5>";
+        
+        forecastCard.innerHTML += "<p>Temperature: " + data.list[forecastDays[i]].main.temp + "\u00b0 F</p>";
+        // forecastCard.innerHTML += "<p>Feels like: " + data.list[forecastDays[i]].main.feels_like + "\u00b0 F</p>";
+        forecastCard.innerHTML += "<p>Humidity: " + data.list[forecastDays[i]].main.humidity + "%</p>";
+        // forecastCard.innerHTML += "<p>Low: " + data.list[forecastDays[i]].main.temp_min + "\u00b0 F</p>";
+        // forecastCard.innerHTML += "<p>High: " + data.list[forecastDays[i]].main.temp_max + "\u00b0 F</p>";
+        forecastCard.innerHTML += "<p>Wind Speed: " + data.list[forecastDays[i]].wind.speed + "mph</p>";
+
+        forecastCard.classList.add("forecastCard");
+
+        document.getElementById("forecast").appendChild(forecastCard);
+    }
+}
+
+function getMonth (dateMonth) {
+
     switch (dateMonth) {
         case "1":
             dateMonth = "January";
@@ -122,48 +190,7 @@ function printDayData (data, i) {
             break;   
     }
 
-    let dateDay = date.substring(8,10);
-
-    console.log ("Icon: " + data.list[i].weather[i].icon);
-
-    cityInfo.innerHTML = "<div class=\"cityHeader\" id=cityHeader></div>";
-
-    const img = document.createElement("img");
-    img.src = "https://openweathermap.org/img/wn/" + data.list[i].weather[i].icon + ".png";
-    document.getElementById("cityHeader").append(img);
-
-    // document.createElement("h1");
-    document.getElementById("cityHeader").innerHTML += "<h1>" + data.city.name + "</h1>";
-
-    cityInfo.innerHTML +="<h4>" + dateMonth + " " + dateDay + ", " + dateYear + "</h4>";
-    cityInfo.innerHTML += "<p>Current temperature: " + data.list[i].main.temp + "\u00b0 F</p>";
-    cityInfo.innerHTML += "<p>Feels like: " + data.list[i].main.feels_like + "\u00b0 F</p>";
-    cityInfo.innerHTML += "<p>Humidity: " + data.list[i].main.humidity + "%</p>";
-    cityInfo.innerHTML += "<p>Today's Low: " + data.list[i].main.temp_min + "\u00b0 F</p>";
-    cityInfo.innerHTML += "<p>Today's High: " + data.list[i].main.temp_max + "\u00b0 F</p>";
-    cityInfo.innerHTML += "<p>Wind Speed: " + data.list[i].wind.speed + "mph</p>";
-
-}
-
-function printForecastCards (data) {
-    //will grab only data indices for future days at 12:00pm.
-    let forecastDays = [5, 13, 21, 29, 37];
-
-    for (i=0 ; i<forecastDays.length; i++) {
-        const forecastCard = document.createElement("div");
-        // forecastCard.innerHTML = "<h1>" + data.city.name + "</h1>";
-        // cityInfo.innerHTML ="<h4>" + dateMonth + " " + dateDay + ", " + dateYear + "</h4>";
-        // forecastCard.innerHTML += "<p>Current temperature: " + data.list[forecastDays[i]].main.temp + "\u00b0 F</p>";
-        // forecastCard.innerHTML += "<p>Feels like: " + data.list[forecastDays[i]].main.feels_like + "\u00b0 F</p>";
-        forecastCard.innerHTML += "<p>Humidity: " + data.list[forecastDays[i]].main.humidity + "%</p>";
-        forecastCard.innerHTML += "<p>Low: " + data.list[forecastDays[i]].main.temp_min + "\u00b0 F</p>";
-        forecastCard.innerHTML += "<p>High: " + data.list[forecastDays[i]].main.temp_max + "\u00b0 F</p>";
-        forecastCard.innerHTML += "<p>Wind Speed: " + data.list[forecastDays[i]].wind.speed + "mph</p>";
-
-        forecastCard.classList.add("forecastCard");
-
-        document.getElementById("forecast").appendChild(forecastCard);
-    }
+    return dateMonth;
 }
 
 //initial population
