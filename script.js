bdb9ed8270cef22cd680bb4a7fdd179693282247
Alejandra Camacho <alejandra.e.bernabel@gmail.com>
2023-08-31 19:10:19 -0400

    Deleted console logs in js

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ function getLatLong (cityEntered) {
 function getWeather(lat, lon) {
     //open weather needs lat, lon, and API key.
     let requestURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=706edb09d7da2e400737ec394e01c5cb&units=imperial";
-    console.log(requestURL);
 
     fetch(requestURL)
     .then (function (response){
